a3ebda21ab5ec225ebf42e44968bce2e6fb43f51
himanshu0089 <88453380+himanshu0089@users.noreply.github.com>
2024-08-24 22:22:07 +0530

    Project setup

---
+++ vite-project/src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+function Footer() {
+  return (
+    <div>
+      <footer>
+      <div>
+        <h2>Connect me through</h2>
+      </div>
+      <div>
+        <Link className="ref">Github</Link>
+        <Link className="ref">Linkedin</Link>
+      </div>
+
+      <div className="signature">
+        {" "}
+        <h3>Designed by Pir@te</h3>
+      </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
